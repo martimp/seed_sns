839a4d7419f47e495d873543ad28b05dd20cfb98
martimp <pishukakkin@gmail.com>
2018-03-12 12:38:29 +0900

    初回コミット

---
+++ dbconnect.php
@@ -0,0 +1,7 @@
+<?php
+    $dsn = 'mysql:dbname=seed_sns;host=localhost';
+    $user = 'root';
+    $password = '';
+    $dbh = new PDO($dsn, $user, $password);
+    $dbh->query('SET NAMES utf8');
+?>
